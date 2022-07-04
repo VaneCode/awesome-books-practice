4cd06ba9eae9ba4b3957872a99d3970702a4c7f7
vanessaBonito <vanebonito@hotmail.com>
2022-07-03 22:57:31 -0500

    Add removeBook function

--- app/js/books-objects.js
+++ app/js/books-objects.js
@@ -1,72 +1,98 @@
-//Variables and cons declaration
+// Variables and cons declaration
 const booksUl = document.querySelector('#books-list-ul');
 const addBookFrm = document.querySelector('#add-book-frm');
 
-
 // Books array of objects book
-const books = [
-    {
-        title: 'Lorem ipsum',
-        author: 'Testeroo Testyy'
-    },
-    {
-        title: 'Second book',
-        author: 'Testeroo Testyy'
-    }
+let books = [
+  {
+    id: 1,
+    title: 'Lorem ipsum',
+    author: 'Testeroo Testyy',
+  },
+  {
+    id: 2,
+    title: 'Second book',
+    author: 'Testeroo Testyy',
+  },
 ];
 
-//--------------------FUNCTIONS-------------------------------//
+// --------------------FUNCTIONS-------------------------------//
 
-//Function to generate an li element for book ul list
-const generateBooksLi = (book) => 
-{   
-    const bookLi = document.createElement('li');
-    const htmlLi = `<li>
-          <div>
-              <p id="title">${book.title}</h3>
-              <p id="author">${book.author}</h4>
-              <button id="delete-book-btn" type="button">Remove</button>
-              <hr>
-          </div>
-      </li>`;
-    bookLi.innerHTML = htmlLi
-    return bookLi;  
-}
+// Function to generate an li element for book ul list
+const generateBooksLi = (book) => {
+  // Create elements
+  const bookLi = document.createElement('li');
+  const divBook = document.createElement('div');
+  const pTitle = document.createElement('p');
+  const pAuthor = document.createElement('p');
+  const btnRemove = document.createElement('button');
+  const hrDeco = document.createElement('hr');
 
+  // Add text to elements
+  pTitle.textContent = `${book.title}`;
+  pAuthor.textContent = `${book.author}`;
+  btnRemove.textContent = 'Remove';
+  // Add atributes
+  btnRemove.setAttribute('id', `${book.id}`);
 
-//Function to add a new book to the array books
-const addNewBook = ()=>{
-    //Get book's information from the add-book-frm form
-    const title = document.querySelector('#book-title').value;
-    const author = document.querySelector('#book-author').value;
-    //Create new book object
-    var newBook = new Object();
-    newBook.title = title;
-    newBook.author = author;
-    //Add the new book object to the books array
-    books.push(newBook);
-    booksUl.appendChild(generateBooksLi(newBook));
-}
+  // Build li
+  divBook.appendChild(pTitle);
+  divBook.appendChild(pAuthor);
+  divBook.appendChild(btnRemove);
+  divBook.appendChild(hrDeco);
+  bookLi.appendChild(divBook);
 
-//Function to remove a book from the array books
+  return bookLi;
+};
 
+// Function to create booksUl
+const createBooksUl = () => {
+  // Clean bookUl
+  booksUl.textContent = ' ';
+  // Generate ul
+  books.forEach((book) => {
+    booksUl.appendChild(generateBooksLi(book));
+  });
+};
 
-//--------------------------EVENTS--------------------------------------//
-addBookFrm.addEventListener('submit',(e)=>{
-    //Prevent actual submit
-    e.preventDefault();
+// Function to remove a book from the array books
+const removeBook = (i) => {
+  books = books.filter((book) => Number(book.id) !== Number(i));
+  createBooksUl();
+};
 
-    //Call function to add a book
-    addNewBook();
-    console.log(books);
-    
-    
+// Function to add a new book to the array books
+const addNewBook = () => {
+  // Calculate book id
+  const id = books.length + 1;
+  // Get book's information from the add-book-frm form
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  // Create new book object
+  const newBook = {};
+  newBook.id = id;
+  newBook.title = title;
+  newBook.author = author;
+  // Add the new book object to the books array
+  books.push(newBook);
+  createBooksUl();
+};
 
+// --------------------------EVENTS--------------------------------------//
+addBookFrm.addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Call function to add a book
+  addNewBook();
 });
 
-window.addEventListener('load', ()=>{
-    books.forEach((book)=>{
-        booksUl.appendChild(generateBooksLi(book));
-       })
+// Add eventListener to btnRemove
+booksUl.addEventListener('click', (e) => {
+  const { id } = e.target;
+  removeBook(id);
 });
 
+window.addEventListener('load', () => {
+  createBooksUl();
+});
