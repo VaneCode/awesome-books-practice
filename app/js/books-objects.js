742119ad25caae6420e2448327bf3243174c69cd
vanessaBonito <vanebonito@hotmail.com>
2022-07-03 23:16:44 -0500

    Add localstorage

--- app/js/books-objects.js
+++ app/js/books-objects.js
@@ -4,95 +4,109 @@ const addBookFrm = document.querySelector('#add-book-frm');
 
 // Books array of objects book
 let books = [
-  {
-    id: 1,
-    title: 'Lorem ipsum',
-    author: 'Testeroo Testyy',
-  },
-  {
-    id: 2,
-    title: 'Second book',
-    author: 'Testeroo Testyy',
-  },
+    {
+        id: 1,
+        title: 'Lorem ipsum',
+        author: 'Testeroo Testyy',
+    },
+    {
+        id: 2,
+        title: 'Second book',
+        author: 'Testeroo Testyy',
+    },
 ];
 
 // --------------------FUNCTIONS-------------------------------//
 
 // Function to generate an li element for book ul list
 const generateBooksLi = (book) => {
-  // Create elements
-  const bookLi = document.createElement('li');
-  const divBook = document.createElement('div');
-  const pTitle = document.createElement('p');
-  const pAuthor = document.createElement('p');
-  const btnRemove = document.createElement('button');
-  const hrDeco = document.createElement('hr');
+    // Create elements
+    const bookLi = document.createElement('li');
+    const divBook = document.createElement('div');
+    const pTitle = document.createElement('p');
+    const pAuthor = document.createElement('p');
+    const btnRemove = document.createElement('button');
+    const hrDeco = document.createElement('hr');
 
-  // Add text to elements
-  pTitle.textContent = `${book.title}`;
-  pAuthor.textContent = `${book.author}`;
-  btnRemove.textContent = 'Remove';
-  // Add atributes
-  btnRemove.setAttribute('id', `${book.id}`);
+    // Add text to elements
+    pTitle.textContent = `${book.title}`;
+    pAuthor.textContent = `${book.author}`;
+    btnRemove.textContent = 'Remove';
+    // Add atributes
+    btnRemove.setAttribute('id', `${book.id}`);
 
-  // Build li
-  divBook.appendChild(pTitle);
-  divBook.appendChild(pAuthor);
-  divBook.appendChild(btnRemove);
-  divBook.appendChild(hrDeco);
-  bookLi.appendChild(divBook);
+    // Build li
+    divBook.appendChild(pTitle);
+    divBook.appendChild(pAuthor);
+    divBook.appendChild(btnRemove);
+    divBook.appendChild(hrDeco);
+    bookLi.appendChild(divBook);
 
-  return bookLi;
+    return bookLi;
 };
 
 // Function to create booksUl
 const createBooksUl = () => {
-  // Clean bookUl
-  booksUl.textContent = ' ';
-  // Generate ul
-  books.forEach((book) => {
-    booksUl.appendChild(generateBooksLi(book));
-  });
+    // Clean bookUl
+    booksUl.textContent = ' ';
+    // Generate ul
+    books.forEach((book) => {
+        booksUl.appendChild(generateBooksLi(book));
+    });
 };
 
 // Function to remove a book from the array books
 const removeBook = (i) => {
-  books = books.filter((book) => Number(book.id) !== Number(i));
-  createBooksUl();
+    books = books.filter((book) => Number(book.id) !== Number(i));
+    //Local storage
+    localStorage.setItem('books', JSON.stringify(books));
+    createBooksUl();
 };
 
 // Function to add a new book to the array books
 const addNewBook = () => {
-  // Calculate book id
-  const id = books.length + 1;
-  // Get book's information from the add-book-frm form
-  const title = document.querySelector('#book-title').value;
-  const author = document.querySelector('#book-author').value;
-  // Create new book object
-  const newBook = {};
-  newBook.id = id;
-  newBook.title = title;
-  newBook.author = author;
-  // Add the new book object to the books array
-  books.push(newBook);
-  createBooksUl();
+    // Calculate book id
+    const id = books.length + 1;
+    // Get book's information from the add-book-frm form
+    const title = document.querySelector('#book-title').value;
+    const author = document.querySelector('#book-author').value;
+    // Create new book object
+    const newBook = {};
+    newBook.id = id;
+    newBook.title = title;
+    newBook.author = author;
+    // Add the new book object to the books array
+    books.push(newBook);
+    //Local storage
+    localStorage.setItem('books', JSON.stringify(books));
+    createBooksUl();
 };
 
 // --------------------------EVENTS--------------------------------------//
 addBookFrm.addEventListener('submit', (e) => {
-  // Prevent actual submit
-  e.preventDefault();
+    // Prevent actual submit
+    e.preventDefault();
 
-  // Call function to add a book
-  addNewBook();
+    // Call function to add a book
+    addNewBook();
 });
 
 // Add eventListener to btnRemove
 booksUl.addEventListener('click', (e) => {
-  const { id } = e.target;
-  removeBook(id);
+    if (e.target.tagName === 'BUTTON') {
+        const { id } = e.target;
+        removeBook(id);
+    }
 });
 
 window.addEventListener('load', () => {
-  createBooksUl();
+    //Local storage
+    if(localStorage.getItem('books')){
+        books = JSON.parse(localStorage.getItem('books'));
+    }else{
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    //Create booksUl
+    createBooksUl();
 });
